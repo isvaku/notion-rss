a4ca49f3bc2236ef3ce512c67f4f5aa0d7c6daff
Rigoberto Quezada <isvaku@hotmail.com>
2024-06-28 13:55:49 -0600

    Update MongoDB connection URL format

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,19 +2,16 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   const MONGO_HOST = process.env.MONGO_HOST || "localhost";
-  const MONGO_PORT = process.env.MONGO_PORT || "27017";
   const MONGO_USER = process.env.MONGO_USER;
   const MONGO_PASS = process.env.MONGO_PASS;
   const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 
-  let mongoUrl = "mongodb://";
+  let mongoUrl = "mongodb+srv://";
   // Check user/pass
   if (MONGO_USER && MONGO_PASS)
     mongoUrl = `${mongoUrl}${MONGO_USER}:${MONGO_PASS}@`;
   // Add host
   mongoUrl = `${mongoUrl}${MONGO_HOST}`;
-  // Check port
-  if (MONGO_PORT) mongoUrl = `${mongoUrl}:${MONGO_PORT}`;
   // Add DB name
   mongoUrl = `${mongoUrl}/${MONGO_DB_NAME}`;
 
